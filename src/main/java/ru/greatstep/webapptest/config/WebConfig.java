fc6ac6dedf646532d260b455c2134b20110e6c95
datkachenko <datkachenko@rtech.ru>
2024-04-07 23:39:09 +0400

    заменен сервер на netty и добавлен configureBlockingExecution для ограничения числа одновременных запросов

--- src/main/java/ru/greatstep/webapptest/config/WebConfig.java
+++ src/main/java/ru/greatstep/webapptest/config/WebConfig.java
@@ -1,7 +1,11 @@
 package ru.greatstep.webapptest.config;
 
+import static java.util.Objects.nonNull;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.task.SimpleAsyncTaskExecutor;
+import org.springframework.lang.NonNull;
 import org.springframework.web.reactive.config.BlockingExecutionConfigurer;
 import org.springframework.web.reactive.config.EnableWebFlux;
 import org.springframework.web.reactive.config.WebFluxConfigurer;
@@ -10,11 +14,19 @@ import org.springframework.web.reactive.config.WebFluxConfigurer;
 @EnableWebFlux
 public class WebConfig implements WebFluxConfigurer {
 
+    @Value("${virtual}")
+    private Boolean isVirtual;
+    @Value("${limit}")
+    private Integer limit;
+
     @Override
-    public void configureBlockingExecution(BlockingExecutionConfigurer configurer) {
+    public void configureBlockingExecution(@NonNull BlockingExecutionConfigurer configurer) {
         var executor = new SimpleAsyncTaskExecutor();
-        executor.setConcurrencyLimit(1);
-        executor.setVirtualThreads(false);
+        executor.setConcurrencyLimit(nonNull(limit) && limit > 200 ? limit : 200);
+        var factory = isVirtual
+                ? Thread.ofVirtual().factory()
+                : Thread.ofPlatform().factory();
+        executor.setThreadFactory(factory);
         configurer.setExecutor(executor);
     }
 
